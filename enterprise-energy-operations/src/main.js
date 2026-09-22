3874c34d77e2e7ea727ad9c104ca0db4a32ff7b8
poluru-labs <subrahmanyam.poluru@gmail.com>
2026-09-21 23:44:31 -0500

    required project files added for enegy operations

---
+++ enterprise-energy-operations/src/main.js
@@ -0,0 +1,5 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@poluru-labs/enterprise-design-system-wc/tokens.css';
+import '@poluru-labs/enterprise-design-system-wc';
+import './styles/terra.css';
+import './terra.js';
