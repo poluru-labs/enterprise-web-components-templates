6e2a8620edbf35a59641cf57f53914eda07570a0
poluru-labs <subrahmanyam.poluru@gmail.com>
2026-09-23 22:07:23 -0500

    footer section added

--- enterprise-customer-success/src/test/setup.js
+++ enterprise-customer-success/src/test/setup.js
@@ -1,1 +1,5 @@
 import '@poluru-labs/enterprise-design-system-wc';
+
+if (typeof CSSStyleSheet !== 'undefined' && typeof CSSStyleSheet.prototype.replaceSync !== 'function') {
+  CSSStyleSheet.prototype.replaceSync = function replaceSync() {};
+}
